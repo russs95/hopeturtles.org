8966e3956b905aeb73cbaf119ea5581871e84a9d
Human2Human <omahsilabumi@gmail.com>
2025-10-07 19:07:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,45 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  // Always read file fresh from disk so updates show immediately
-  const filePath = path.join(__dirname, 'index.html');
-  fs.readFile(filePath, (err, data) => {
+  // Map request to file path
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath).toLowerCase();
+
+  // Default content type
+  let contentType = 'text/html';
+  switch (ext) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+    case '.jpeg':
+      contentType = 'image/jpeg';
+      break;
+  }
+
+  // Read the file and serve it
+  fs.readFile(filePath, (err, content) => {
     if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.end('Server Error');
-      return;
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
     }
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.end(data);
   });
 });
 
