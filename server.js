7a4a3bc9b54369d337094e90605b7491305d582d
Human2Human <omahsilabumi@gmail.com>
2025-10-07 18:54:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const index = fs.readFileSync(path.join(__dirname, 'index.html'));
-
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-  res.end(index);
+  // Always read file fresh from disk so updates show immediately
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Server Error');
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end(data);
+  });
 });
 
-// Listen on port 80 for http://<ip>/
 server.listen(3000, '127.0.0.1', () => {
   console.log('Landing page listening on http://127.0.0.1:3000');
 });
